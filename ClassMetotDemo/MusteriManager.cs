dc99cc350775892b241357d62beb523765e0314e
Berkay Şahin <berkaysah26@gmail.com>
2021-08-12 16:58:50 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi: "+ musteri.Ad+" "+musteri.Soyad);
+        }
+        public void Ekle(Musteri[] musteri)
+        {
+            for(int i=0;i<musteri.Length;i++)
+            Console.WriteLine(i+1+")Müşteri eklendi: " + musteri[i].Ad + " " + musteri[i].Soyad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi: " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void Sil(Musteri[] musteriler)
+        {
+            for(int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine("Müşteri silindi: " + musteriler[i].Ad + " " + musteriler[i].Soyad);
+            }
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            int i = 1;
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad+" "+musteri.Soyad);
+                i++;
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Berkay";
+            musteri1.Soyad = "Şahin";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Ahmet";
+            musteri2.Soyad = "Sezer";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Engin";
+            musteri3.Soyad = "Demiroğ";
+
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            
+            
+            Console.WriteLine("-----Ekleme-----");
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            Console.WriteLine("-----Toplu Ekleme-----");
+            musteriManager.Ekle(musteriler);
+
+            
+            
+            Console.WriteLine("-----Silme-----");
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Sil(musteri3);
+
+            Console.WriteLine("-----Toplu Silme-----");
+            musteriManager.Sil(musteriler);
+
+
+
+
+            Console.WriteLine("Listeleme");
+            musteriManager.Listele(musteriler);
+
+
+
+
+        }
+    }
+}
